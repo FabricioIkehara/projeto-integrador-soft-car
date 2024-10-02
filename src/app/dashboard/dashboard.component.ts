07841177de47e406e68841970b40db5c2fa41e4e
FabricioIkehara <fah.hiroshi@gmail.com>
2024-10-01 23:28:12 -0300

    fechandopriemiro dia

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuBarComponent } from "../components/menu-bar/menu-bar.component";
+import { MenuSideComponent } from "../components/menu-side/menu-side.component";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   standalone: true,
   styleUrl: './dashboard.component.css',
-  imports: [CommonModule, MenuBarComponent]
+  imports: [CommonModule, MenuBarComponent, MenuSideComponent]
 })
 export class DashboardComponent {}
