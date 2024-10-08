14b97d413e4dc73a1e0376810f749051e6b6ceb1
FabricioIkehara <fah.hiroshi@gmail.com>
2024-10-08 15:34:30 -0300

    small-card

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuBarComponent } from "../components/menu-bar/menu-bar.component";
 import { MenuSideComponent } from "../components/menu-side/menu-side.component";
+import { SmallCardComponent } from '../components/small-card/small-card.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   standalone: true,
   styleUrl: './dashboard.component.css',
-  imports: [CommonModule, MenuBarComponent, MenuSideComponent]
+  imports: [CommonModule, MenuBarComponent, MenuSideComponent, SmallCardComponent]
 })
 export class DashboardComponent {}
